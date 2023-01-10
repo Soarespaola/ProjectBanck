045f015337bdbd3b9830120e208aa4a5bc4eff67
Paola Soares <paolaalcantara1@yahoo.com.br>
2023-01-10 18:00:44 -0300

    Configura app

---
+++ ProjectBankUITests/ProjectBankUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ProjectBankUITestsLaunchTests.swift
+//  ProjectBankUITests
+//
+//  Created by Paola Alcantara Soares on 10/01/23.
+//
+
+import XCTest
+
+class ProjectBankUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
