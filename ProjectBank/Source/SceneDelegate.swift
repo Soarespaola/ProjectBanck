d6af7fa4b7c4ac0d1d33edb35d68765dc3e39f09
Paola Soares <paolaalcantara1@yahoo.com.br>
2023-01-10 18:04:38 -0300

    configura app

--- ProjectBank/Source/SceneDelegate.swift
+++ ProjectBank/Source/SceneDelegate.swift
@@ -13,12 +13,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let vc: UIViewController = UIViewController()
-        let nav = UINavigationController(rootViewController: nav)
+        let nav = UINavigationController(rootViewController: vc)
+        window.rootViewController = nav
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
